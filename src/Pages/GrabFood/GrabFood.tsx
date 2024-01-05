cba2f66e98e0d27c897b8b247206b6d5a1325863
jsandiegosnrshopping01 <129921568+jsandiegosnrshopping01@users.noreply.github.com>
2024-01-05 11:14:09 +0800

    Portal Table
    Aligning Header table when it has a value or no data
    Correction of corresponding header per table
    Aligning value of every table by different data

--- src/Pages/GrabFood/GrabFood.tsx
+++ src/Pages/GrabFood/GrabFood.tsx
@@ -608,6 +608,7 @@ const GrabFood = () => {
                         <PortalTable 
                           portal={portal}
                           loading={loading}
+                          merchant='GrabFood'
                         />
                       </Box>
                     </Fade>
